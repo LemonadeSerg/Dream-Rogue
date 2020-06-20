5d0116cd71e2d151da11867018018625437e904c
LemonadeSerg <chris.jon.sharp@gmail.com>
2020-06-20 11:52:12 +0100

    Graphical update
    Little update to ui and sound with a couple asset packages

---
+++ Assets/Scripts/Title Scripts/BackgroundAnimation.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundAnimation : MonoBehaviour
+{
+    public float changeSpeed = 0.1f;
+
+    public SpriteRenderer Background, Overlay;
+    private float r = 1f, g = 0f, b = 0f;
+
+    public int stage = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (stage == 0)
+        {
+            g += changeSpeed;
+
+            if (g >= 1)
+            {
+                stage = 1;
+            }
+        }
+        if (stage == 1)
+        {
+            r -= changeSpeed;
+            if (r <= 0)
+            {
+                stage = 2;
+            }
+        }
+        if (stage == 2)
+        {
+            b += changeSpeed;
+            if (b >= 1)
+            {
+                stage = 3;
+            }
+        }
+        if (stage == 3)
+        {
+            g -= changeSpeed;
+            if (g <= 0)
+            {
+                stage = 4;
+            }
+        }
+        if (stage == 4)
+        {
+            r += changeSpeed;
+            if (r >= 1)
+            {
+                stage = 5;
+            }
+        }
+        if (stage == 5)
+        {
+            b -= changeSpeed;
+            if (b <= 0)
+            {
+                stage = 0;
+            }
+        }
+        Background.color = new Color(r, g, b);
+        Overlay.color = new Color(1 - r, 1 - g, 1 - b);
+    }
+}
---
+++ Assets/Scripts/Title Scripts/TitleMenu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class TitleMenu : MonoBehaviour
+{
+    public GameObject[] Menus;
+    public InputField worldNameIn;
+    public InputField seedIn;
+    public int roomMaker;
+    public WorldGenerationManager worldGenManager;
+    public DateTime worldGenStartTime;
+    public AudioSource clicksound;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void selectMenu(int menuID)
+    {
+        for (int i = 0; i < Menus.Length; i++)
+        {
+            if (menuID == i)
+            {
+                Menus[i].SetActive(true);
+            }
+            else
+            {
+                Menus[i].SetActive(false);
+            }
+        }
+    }
+
+    public void Play_Button_Click()
+    {
+        clicksound.Play();
+        selectMenu(1);
+    }
+
+    public void Settings_Button_Click()
+    {
+        clicksound.Play();
+        selectMenu(2);
+    }
+
+    public void Exit_Button_Click()
+    {
+        clicksound.Play();
+        Application.Quit();
+    }
+
+    public void Back_Button_Click()
+    {
+        clicksound.Play();
+        selectMenu(0);
+    }
+
+    public void worldNameTyping(string text)
+    {
+        print(text);
+    }
+
+    public void RoomMaker()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void genNewWorld()
+    {
+        worldGenStartTime = System.DateTime.Now;
+        worldGenManager.gen(int.Parse(seedIn.text), 4);
+        print("Time taken to generate world :" + (worldGenStartTime - System.DateTime.Now).Seconds.ToString() + "." + (System.DateTime.Now - worldGenStartTime).Milliseconds.ToString() + " Seconds");
+    }
+
+    public void loadLevel()
+    {
+        ScenePersistantData.worldName = seedIn.text;
+        SceneManager.LoadScene(2);
+    }
+}
